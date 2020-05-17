72767f48c8d23d7b7a7ca5e76b499a03a2608d11
garenk95 <garen.kantarci1@gmail.com>
2020-05-17 16:21:14 -0700

    cOmmITinG

--- server/server.js
+++ server/server.js
@@ -13,29 +13,55 @@ app.use(cookieParser());
 //Adds a new vehicle run to the system, should return a cookie called USER=[name] 
 //that would be included for the other commands. Width is the width of the vehicle in cm.
 app.get(`/register`, (req, res) => {
-    res.send(`The app is now starting.\n
-    The name is: ${req.query.name}\n
-    The distance between the wheels (width) is: ${req.query.width}cm\n`);
+    res.send(`session starting!
+    \nThe name of the cookie (session): ${req.query.name}
+    \nThe distance between the wheels (width) is: ${req.query.width}cm\n`);
 });
 
 //THIS IS THE ONLY DATA THAT NEEDS TO BE PROCESSED
 //EVERYTHING ELSE IS STORED AND DISPLAYED
 //Records the speed of the left and right wheel in cm/sec for that vehicle in the current session
 app.get('/wheels', (req, res) => {
-    res.send(`The left wheel is moving at ${req.query.left} cm/sec\n
-    The right wheel is moving at ${req.query.right} cm/sec\n`);
+    res.send(`The left wheel is moving at ${req.query.left} cm/sec
+    \nThe right wheel is moving at ${req.query.right} cm/sec\n`);
 });
 
 //Records the result of the echo sensor in cm for the vehicle in the current session
 app.get('/echo', (req, res) => {
     res.send(`The distance is: ${req.query.dist}\n`);
 });
+
+
 app.get('/line', (req, res) => {
     output = "";
-    for (key in query) {
+    for (key in req.query) {
         //instead of outputting, write this to database
         output = output + `For the item: ${key} the value is ${req.query[key]}` + `\n`;
     }
     res.send(output);
 });
 
+app.get(`/other`, (req, res) => {
+    output = "";
+    for (key in req.query) {
+        //instead of outputting, write this to database
+        output = output + `For the item: ${key} the value is ${req.query[key]}` + `\n`;
+    }
+    res.send(output);
+});
+
+app.get(`/end`, (req, res) => {
+    res.send(`session ending!`);
+});
+
+//these functions store in database? maybe?
+//const store = require('./store');
+//(1)
+app.post(`/startSession/:cookie/:width/:time`, (req, res) => {
+    //make new table with cookie name?
+});
+
+//(2)
+app.post(`/addWheels/:left_w_speed/:right_w_speed/:time`, (req, res) => {
+
+});
